3aac606ec192aef801e8ca1334494e63915b1f79
JUHI <123229965+juhi048@users.noreply.github.com>
2023-09-17 08:16:14 +0530

    SAP labs

---
+++ remove spaces.cpp
@@ -0,0 +1,9 @@
+long long int product(int ar[], int n, long long int mod)
+ {
+     //code here
+     long long int ans = 1;
+        for(int i=0;i<n;i++){
+            ans=(ans*ar[i])%mod;
+        }
+        return ans;
+ }
